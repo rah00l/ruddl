c3ff11feda37a4d8ae6ce6b5c2d4004288ab3588
Jesal Gadhia <jesal7@gmail.com>
2012-04-06 17:13:53 -0700

    RailywayJS Scaffoldig

---
+++ app/controllers/application_controller.js
@@ -0,0 +1,3 @@
+before('protect from forgery', function () {
+    protectFromForgery('8d5b3d075db2b40783f5807925cf4d7e3c6db4d2');
+});
---
+++ db/schema.js
@@ -0,0 +1,9 @@
+define('User', function () {
+    property('email', String, { index: true });
+    property('password', String);
+    property('activated', Boolean, {default: false});
+});
+
+var User = describe('User', function () {
+    property('feed', String);
+});
