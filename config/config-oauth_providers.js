1331986ba2e692d33054aa2fe2497a094754af77
Jesal Gadhia <jesal7@gmail.com>
2012-04-08 19:14:05 -0700

    Adding actual keys for ruddl

--- config/config-oauth_providers.js
+++ config/config-oauth_providers.js
@@ -1,18 +1,10 @@
 module.exports = {
   fb: {
-    appId: '111565172259433',
-    appSecret: '85f7e0a0cc804886180b887c1f04a3c1'
+    appId: '200738486708790',
+    appSecret: '05c0869cf1b6233fa5c236962199fd01'
   },
   twit: {
-    consumerKey: 'JLCGyLzuOK1BjnKPKGyQ',
-    consumerSecret: 'GNqKfPqtzOcsCtFbGTMqinoATHvBcy1nzCTimeA9M0'
-  },
-  github: {
-    appId: '11932f2b6d05d2a5fa18',
-    appSecret: '2603d1bc663b74d6732500c1e9ad05b0f4013593'
-  },
-  instagram: {
-    clientId: 'be147b077ddf49368d6fb5cf3112b9e0',
-    clientSecret: 'b65ad83daed242c0aa059ffae42feddd'
-  },
+    consumerKey: 'xKDkWPIfPKSHVDgvilw',
+    consumerSecret: 'n8SnNTfNxkS8TN86n6izfGDbQel9X1BCsmXhun44pe4'
+  }
 };
