81b74536d0335e58b2909b9475eb3ca4f5a1184f
Jesal Gadhia <jesal7@gmail.com>
2012-04-09 22:05:10 -0700

    new index route, controller and view

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,8 @@
 exports.routes = function (map) {
     map.resources('users');
 
-
-
+	map.root('home#index');
+	
     // Generic routes. Add all your routes below this line
     // feel free to remove generic routes
     map.all(':controller/:action');
