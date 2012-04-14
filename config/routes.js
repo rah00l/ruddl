ccc5b5f22f3e93f185d63a6e3820a10ecaf643f8
Jesal Gadhia <jesal7@gmail.com>
2012-04-13 17:04:44 -0700

    Restarting

--- config/routes.js
+++
@@ -1,9 +0,0 @@
-exports.routes = function (map) {
-    map.resources('users');
-	map.root('home#index');
-	
-    // Generic routes. Add all your routes below this line
-    // feel free to remove generic routes
-    map.all(':controller/:action');
-    map.all(':controller/:action/:id');
-};
