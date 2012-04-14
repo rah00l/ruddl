531ae8525de1e8ef26a47e1ce4371e0375dd08ab
Jesal Gadhia <jesal7@gmail.com>
2012-04-14 15:26:24 -0700

    Adding bootstrap and cleaning up presentation

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+	before_filter :except => [:index]
+
+	def index
+  	end
+
+  	def about
+  	end
+  	
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,6 @@
 class UsersController < ApplicationController
 	before_filter :authenticate_user!, :except => [:index]
+
+	def index
+  	end
 end
