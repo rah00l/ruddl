21961ddf4ec292d1ad824ef81e82a372370ec245
jesalg <jesal7@gmail.com>
2012-10-03 11:36:38 -0700

    Reset

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Ruddl::Application.config.secret_token = '5b252b74fe97040297b0de7389acca66ef883aaa5c372a4d6606323d489119e93b65f36b6d60db6927608423349053423927d34ff58a3e4e08c9bd4a85fce72a'
