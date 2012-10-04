80a5b11b9ad2f0fec46e45d1c53ced0767bd62b2
jesalg <jesal7@gmail.com>
2012-10-03 17:22:20 -0700

    Adding sinatra-contrib to make use of the reloader

--- myapp.rb
+++ myapp.rb
@@ -1,6 +1,14 @@
+require "sinatra/base"
+require "sinatra/reloader"
+
 class MyApp < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
   get "/" do
-    rss = SimpleRSS.parse open('http://slashdot.org/index.rdf')
-    rss.channel.title
+    feed = SimpleRSS.parse open('http://slashdot.org/index.rdf')
+    #http://snippets.aktagon.com/snippets/164-How-to-use-Ruby-and-SimpleRSS-to-parse-RSS-and-Atom-feeds-
+    erb :index
   end
 end
