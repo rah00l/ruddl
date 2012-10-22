5feef3945ee4e428c7c765bdab3980ab364155eb
jesalg <jesal7@gmail.com>
2012-10-22 14:57:35 -0700

    Use name as the id in obj and dom

--- myapp.rb
+++ myapp.rb
@@ -17,7 +17,7 @@ class MyApp < Sinatra::Base
 
   def parse_youtube(item)
     begin
-      rdoc = RuddlDoc.new(item['data']['id'], item['data']['title'], item['data']['media']['oembed']['thumbnail_url'], item['data']['url'], URI.join('http://reddit.com/', item['data']['permalink']))
+      rdoc = RuddlDoc.new(item['data']['name'], item['data']['title'], item['data']['media']['oembed']['thumbnail_url'], item['data']['url'], URI.join('http://reddit.com/', item['data']['permalink']))
     rescue => exception
       puts exception
     end
@@ -33,7 +33,7 @@ class MyApp < Sinatra::Base
       image = album_json['album']['images'][0]['image']['hash']
     end
     ext = (File.extname(image).length == 0) ? '.jpg' : ''
-    rdoc = RuddlDoc.new(item['data']['id'], item['data']['title'], URI.join(host, image+ext), item['data']['url'],URI.join('http://reddit.com/', item['data']['permalink']))
+    rdoc = RuddlDoc.new(item['data']['name'], item['data']['title'], URI.join(host, image+ext), item['data']['url'],URI.join('http://reddit.com/', item['data']['permalink']))
     rdoc
   end
 
@@ -42,7 +42,7 @@ class MyApp < Sinatra::Base
     host = "http://i.qkme.me"
     image = URI(item['data']['url'])
     ext = '.jpg'
-    rdoc = RuddlDoc.new(item['data']['id'], item['data']['title'], URI.join(host, image.path.gsub("/meme/", "").gsub("/", "")+ext), item['data']['url'], URI.join('http://reddit.com/', item['data']['permalink']))
+    rdoc = RuddlDoc.new(item['data']['name'], item['data']['title'], URI.join(host, image.path.gsub("/meme/", "").gsub("/", "")+ext), item['data']['url'], URI.join('http://reddit.com/', item['data']['permalink']))
     rdoc
   end
 
@@ -51,7 +51,7 @@ class MyApp < Sinatra::Base
     title = URI(item['data']['url']).path.gsub('/wiki/','')
     wiki_json = JSON.parse(open("http://en.wikipedia.org/w/api.php?action=query&prop=imageinfo&format=json&iiprop=url&iilimit=1&generator=images&titles=#{title}&gimlimit=1").read)
     begin
-      rdoc = RuddlDoc.new(item['data']['id'], item['data']['title'], wiki_json['query']['pages']['-1']['imageinfo'][0]['url'], item['data']['url'], URI.join('http://reddit.com/', item['data']['permalink']))
+      rdoc = RuddlDoc.new(item['data']['name'], item['data']['title'], wiki_json['query']['pages']['-1']['imageinfo'][0]['url'], item['data']['url'], URI.join('http://reddit.com/', item['data']['permalink']))
       rdoc
     rescue => exception
       puts exception
@@ -84,7 +84,7 @@ class MyApp < Sinatra::Base
     end
 
     if not best_image.nil?
-      rdoc = RuddlDoc.new(item['data']['id'], item['data']['title'], best_image, item['data']['url'], URI.join('http://reddit.com/', item['data']['permalink']))
+      rdoc = RuddlDoc.new(item['data']['name'], item['data']['title'], best_image, item['data']['url'], URI.join('http://reddit.com/', item['data']['permalink']))
       rdoc
     else
       nil
@@ -95,7 +95,7 @@ class MyApp < Sinatra::Base
     rdoc = nil
     if (item['data']['over_18'] == false)
       if (item['data']['url'] =~ /#{['jpg', 'jpeg', 'gif', 'png'].map { |m| Regexp.escape m }.join('|')}/)
-        rdoc = RuddlDoc.new(item['data']['id'], item['data']['title'], item['data']['url'], item['data']['url'], URI.join('http://reddit.com/', item['data']['permalink']))
+        rdoc = RuddlDoc.new(item['data']['name'], item['data']['title'], item['data']['url'], item['data']['url'], URI.join('http://reddit.com/', item['data']['permalink']))
       elsif (item['data']['domain'].include? 'imgur')
         rdoc = parse_imgur(item)
       elsif (item['data']['domain'].include? 'quickmeme' or item['data']['domain'].include? 'qkme')
@@ -125,7 +125,7 @@ class MyApp < Sinatra::Base
 
       @feed = JSON.parse(open("http://www.reddit.com/#{section}.json", "User-Agent" => "ruddl by /u/jesalg").read)
       @feed['data']['children'].each_with_index do |item, index|
-        doc_key = item['data']['id']
+        doc_key = item['data']['name']
         puts "#{index} => #{doc_key}"
         if (@@redis.exists(doc_key))
           puts "#{doc_key} found in cache"
