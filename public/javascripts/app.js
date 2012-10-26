b50a9cbfafbe061b2201382a2e35f4962b7ba06b
Jesal Gadhia <jesal7@gmail.com>
2012-10-25 23:01:44 -0700

    Cleaning up js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -18,7 +18,6 @@
 $(function() {
     var ruddl = (function () {
         var container = $('#container');
-        var updateInterval = 30000;
 
         var ruddl = function () {
             this.calcCols(false);
@@ -28,7 +27,6 @@ $(function() {
                     isAnimated: !Modernizr.csstransitions
                 });
             });
-            //this.startTimer();
         };
 
         var setCols = function(width) {
@@ -45,8 +43,7 @@ $(function() {
         var updateNextURL = function(newElems) {
             var lastElem = newElems[newElems.length-1]
             var uri = new URI($('#load-more').attr('href').replace('#',''));
-            uri.segment(uri.segment.length-1, parseInt(uri.segment(uri.segment.length-1)) + 1);
-            uri.segment(uri.segment.length-2, lastElem.id);
+            uri.segment(uri.segment.length-1, lastElem.id);
             $('#load-more').attr('href', '#'+uri.toString())
         };
 
@@ -71,28 +68,11 @@ $(function() {
                     updateNextURL(newElems);
                 });
             },
-            refreshFeed : function () {
-                this.getUpdate($('.sub-nav dd.active').find('a').attr('href').replace('#/',''));
-                this.calcCols(true);
-            },
             calcCols : function (reloadMasonry) {
                 var width = Math.floor(($(window).width()-70)/3);
                 setCols(width);
                 if(reloadMasonry)
                     container.masonry('reload');
-            },
-            startTimer : function() {
-                parent = this;
-                var counter = updateInterval / 1000;
-
-                var timer = window.setInterval(function() {
-                    $('.timer').html('Next update in: '+ counter-- +'s');
-                }, 1000);
-
-                var interval = window.setInterval(function() {
-                    counter = updateInterval / 1000;
-                    parent.refreshFeed();
-                }, updateInterval);
             }
         };
 
